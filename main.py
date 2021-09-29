d95bc7c4d9a1f456b5537afae0755196efa3a165
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2021-09-29 21:02:01 +0000

    Experimented with inner functions. - Plan, separate again (make an only refill function) - Warning message if something is too low. - Refill material if in dictionary.

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu import MENU, resources
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+def report_menu(content_dict, money):
+    """Displays the resources of the machine"""
+    print("= Resources available =")
+    for resource in content_dict:
+        if resource == "coffee":
+            print("{}: {}g".format(resource,content_dict[resource]))
+        else:    
+            print("{}: {}ml".format(resource,content_dict[resource]))
+    print("money: €{}".format(money))
 
+def refill(content_dict):
+    def water():
+        water = content_dict["water"]
+        if water < 300:
+            water = 300
+            print("Watertank has been refilled to 300ml")
+        else:
+            print("Watertank already filled")
+        return water        
+
+    def milk():
+        milk = content_dict["milk"]
+        if milk < 200:
+            milk = 200
+            print("Milk has been refilled to 200ml")
+        else:
+            print("Already filled 100%")        
+        return milk
+
+    def coffee():
+        coffee = content_dict["coffee"]
+        if coffee < 100:
+            coffee = 100
+            print("Coffee has been refilled to 100g")
+        else:
+            print("Already filled 100%")
+        return coffee
+
+funds = 0.0
+
+refill(resources)
