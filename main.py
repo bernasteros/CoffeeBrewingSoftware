9c3caa7ef5be27002e6e125f0afc4069ab3e4f8c
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2021-09-30 06:48:38 +0000

    Edited refill function

--- main.py
+++ main.py
@@ -10,34 +10,35 @@ def report_menu(content_dict, money):
             print("{}: {}ml".format(resource,content_dict[resource]))
     print("money: €{}".format(money))
 
-def refill(content_dict):
-    def water():
-        water = content_dict["water"]
-        if water < 300:
-            water = 300
-            print("Watertank has been refilled to 300ml")
-        else:
-            print("Watertank already filled")
-        return water        
-
-    def milk():
-        milk = content_dict["milk"]
-        if milk < 200:
-            milk = 200
+def refill(material):
+    """Refills """      
+    selection = input("What do you want to refill? >>> ").lower()    
+    
+    if selection == "water":
+      if material < 300:
+        material = 300
+        print("Watertank has been refilled to 300ml")
+      else:
+          print("Watertank already filled")         
+    elif selection == "milk":
+        if material < 200:
+            material = 200
             print("Milk has been refilled to 200ml")
         else:
             print("Already filled 100%")        
-        return milk
+        return material
 
-    def coffee():
-        coffee = content_dict["coffee"]
-        if coffee < 100:
-            coffee = 100
+    elif selection == "coffee":
+        if material < 100:
+            material = 100
             print("Coffee has been refilled to 100g")
         else:
             print("Already filled 100%")
-        return coffee
+    else:
+        print("Error: No such material to refill")    
+    return material
 
+# Programm Start from here
 funds = 0.0
 
 refill(resources)
